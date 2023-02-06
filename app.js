220268d521f279e1b329d0d89a6df49802a6b04b
faizan dhedhi <120604695+muhammad-faizan-mohsin@users.noreply.github.com>
2023-02-07 01:23:19 +0500

    half compelete yet

---
+++ app.js
@@ -0,0 +1,84 @@
+// Exersice 1
+// Answer 1
+
+// var city = prompt("Enter your city name");
+
+// if(city === "karachi"){
+//    document.write("Welcome to city of lights")
+// }
+
+
+//Answer 2
+
+// var gender = prompt("Enter your gander");
+
+// if(gender === "male"){
+//     document.write("Good Morning Sir.")}
+// if(gender === "female"){
+//     document.write("Good Morning Ma’am..")
+// }
+
+
+// Answer 3
+
+// var gender = prompt("Enter color");
+
+// if(gender == "Red"){
+//     document.write("Must Stop")}
+// if(gender == "Yellow"){
+//     document.write("Ready to move")
+// }
+// if(gender == "Green"){
+//     document.write("Move now")
+// }
+
+
+
+// Answer 4
+// var fuel = prompt("Enter your fuel in leter ");
+// if (fuel < "0.25"){
+//     document.write("Please refill the fuel in your car")
+// }
+
+
+// Answer 5
+var a = 4;
+if (++a === 5){
+alert("given condition for variable a is true");
+}
+
+var b = 82;
+if (b++ === 83){
+alert("given condition for variable b is true");
+}
+var c = 12;
+if (c++ === 13){
+alert("condition 1 is true");
+}
+if (c === 13){
+alert("condition 2 is true");
+}
+if (++c < 14){
+alert("condition 3 is true");
+}
+if(c === 14){
+alert("condition 4 is true");
+}
+
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost){
+alert("The cost equals");
+}
+ if (true){
+alert("True");
+}
+if (false){
+alert("False");
+}
+
+
+if("car" < "cat"){
+alert("car is smaller than cat");
+}
